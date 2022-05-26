4a1383ef4562af7dea4029154e891e228ada2859
MikoBie <profesor_floretu@op.pl>
2022-05-26 02:51:03 +0200

    N6: very first draft. HW4: minor fix. P2: oven-ready. TODO: Spell-check.

---
+++ notebooks/mm.jl
@@ -0,0 +1,2 @@
+{"name": "Marianna","age": 17,"interests": [{"name": "physics","disciplines": ["quantum physics","string theory"]},{"name": "sport","disciplines": ["fishing","football"]}]}
+{"name": "Marian","age": 15,"interests": [{"name": "sport","disciplines": ["football"]}]}
