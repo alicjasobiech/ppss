5597435389e235a76b7dd39a0fdd1d4f4a02d447
MikoBie <profesor_floretu@op.pl>
2024-03-01 16:05:27 +0100

    Update: ruff-formatting and pre-commit hooks.

--- tests/setup.py
+++ tests/setup.py
@@ -1,11 +1,13 @@
 import setuptools
 
-setuptools.setup(name='ppss_tests',
-		 version='0.1',
-                 description='Package with tests for homeworks assignments in Programming in Python for Social Scientists',
-                 ulr='https://github.com/MikoBie/ppss',
-                 author='Mikołaj Biesaga',
-                 install_requires=[],
-                 author_email='m.biesaga@uw.edu.pl',
-                 packages=setuptools.find_packages(),
-                 zip_safe=False)
+setuptools.setup(
+    name="ppss_tests",
+    version="0.1",
+    description="Package with tests for homeworks assignments in Programming in Python for Social Scientists",
+    ulr="https://github.com/MikoBie/ppss",
+    author="Mikołaj Biesaga",
+    install_requires=[],
+    author_email="m.biesaga@uw.edu.pl",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
